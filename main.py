5c73b9426dc8afcd9e65cec62882635af20de0e0
Moewen-dev <Tom.Penning@pm.me>
2024-08-24 01:11:11 +0200

    switched from pickling files to store users to sqlite and made everything finally work

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pickle
+import sqlite3
 import hashlib
 import sys
 import os
@@ -10,94 +10,101 @@ class User:
     password_hash = str
 
 
-def print_menu(logged):
-    menu_not_logged = """
-    ------Login-test------
-    | 1. Signup          |
-    | 2. Login           |
-    | 3. Exit            |
-    ----------------------
-    """
-    if not logged:
-        print(menu_not_logged)
-
-
-def signup():
-    new_user = User
-    new_user.username = input("Username:\n")
-    email = input("Email:\n")
-    if input("Confirm Email:\n") == email:
-        new_user.email = email
-    else:
-        print("Email confirmation failed")
-        return False
-    password = input("Password:\n")
-    if input("Confirm Password:\n") != password:
-        print("Password confirmation failed")
-        return False
-    passhash = hashlib.sha512(password.encode())
-    new_user.password_hash = passhash.hexdigest()
-    return new_user
-
-
-def login(user):
-    if user.username != input("Username:\n"):
-        print("Wrong username")
-        return False
-    passhash = hashlib.sha512(input("Password:\n").encode())
-    if user.password_hash != passhash.hexdigest():
-        print("Wrong password")
-        return False
-    return True
-
-
-def save(user):
-    with open("user", 'wb') as file:
-        pickle.dump(user, file)
-    file.close()
-    print(f"{user.username} saved")
-
-
-def load():
-    with open("user", 'rb') as file:
-        user = pickle.load(file)
-    file.close()
-    print(user)
-    return user
-
-
 if __name__ == '__main__':
+    try:
 
-    logged_in = False
+        # init user database
+        conn = sqlite3.connect("users.db")
+        cur = conn.cursor()
 
-    while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print_menu(logged_in)
-        if not logged_in:
-            try:
-                choice = input("Choice:\n")
-                match int(choice):
-                    case 1:
-                        current_user = signup()
-                        save(current_user)
-                        logged_in = True
-                        input("")
-                    case 2:
-                        try:
-                            current_user = load()
-                        except FileExistsError:
-                            print("Could not load userdata")
-                            continue
-                        logged_in = login(current_user)
-                        input("")
-                    case 3:
-                        print("Bye!")
-                        sys.exit(0)
-            except ValueError:
-                print("Invalid Choice")
+        def print_menu(logged):
+            menu_not_logged = """
+            ------Login-test------
+            | 1. Signup          |
+            | 2. Login           |
+            | 3. Exit            |
+            ----------------------
+            """
+            if not logged:
+                print(menu_not_logged)
+
+        def signup():
+            new_user = User
+            new_user.username = input("Username:\n")
+            email = input("Email:\n")
+            if input("Confirm Email:\n") == email:
+                new_user.email = email
+            else:
+                print("Email confirmation failed")
+                return False
+            password = input("Password:\n")
+            if input("Confirm Password:\n") != password:
+                print("Password confirmation failed")
+                return False
+            pw_hash = hashlib.sha512(password.encode())
+            new_user.password_hash = pw_hash.hexdigest()
+            return new_user
+
+        def login():
+            username = input("Username:\n")
+            usernames = cur.execute("SELECT user_id, username FROM users").fetchall()
+            for u in usernames:
+                if u[1] == username:
+                    password = hashlib.sha512(input("Password:\n").encode()).hexdigest()
+
+                    db_pw = cur.execute(f"SELECT user_id, password_hash FROM users").fetchall()
+                    for p in db_pw:
+                        if p[1] == password:
+                            return True
+                        print("Wrong password\n")
+                        return False
+            print("User not found\n")
+            return False
+
+
+        def save(user):
+            data = (user.username, user.email, user.password_hash)
+            cur.execute('INSERT INTO users(username, email, password_hash) VALUES (?, ?, ?)', data)
+            conn.commit()
+
+        logged_in = False
+
+
+        if cur.execute("SELECT name FROM sqlite_master").fetchone() != ('users',) :
+            cur.execute('''CREATE TABLE users
+            (user_id INTEGER PRIMARY KEY,
+            username,
+            email,
+            password_hash)''')
+            print(f"Created {cur.execute("SELECT name FROM sqlite_master").fetchone()}")
+
+        while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print_menu(logged_in)
+            if not logged_in:
+                try:
+                    choice = input("Choice:\n")
+                    match int(choice):
+                        case 1:
+                            n_user = signup()
+                            save(n_user)
+                            print(f"Successfully registered {n_user.username}\n")
+                            input("")
+                        case 2:
+                            logged_in = login()
+                        case 3:
+                            print("Bye!")
+                            sys.exit(0)
+                except ValueError:
+                    print("Invalid Choice")
+                    input("")
+                    continue
+            else:
+                print("Wow you logged in\nBye now :)")
                 input("")
-                continue
-        else:
-            print("Wow you logged in\nBye now :)")
-            input("")
-            sys.exit(0)
+                sys.exit(0)
+
+    except KeyboardInterrupt:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Used the fast way out huh...\nBye")
+        sys.exit(0)
